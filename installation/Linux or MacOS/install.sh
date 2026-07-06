1dd7732c4669f4631fb06a4e11b2ee27fa0cf886
keyhan-112 <keyhan.kh1391@gmail.com>
2026-07-06 21:54:58 +0330

    Update install.sh

--- installation/Linux or MacOS/install.sh
+++ installation/Linux or MacOS/install.sh
@@ -4,6 +4,7 @@ set -e
 
 # Install Git
 if ! command -v git >/dev/null; then
+    echo "install git..."
     if command -v apt >/dev/null; then
         sudo apt update
         sudo apt install -y git curl build-essential \
@@ -19,28 +20,37 @@ if ! command -v git >/dev/null; then
         echo "Unsupported distro"
         exit 1
     fi
+    echo "installed!!"
 fi
 
 read -p "Install path: " PATH_INSTALL
 
+echo "Clone of repositories..."
 git clone https://github.com/erfan114/clicopy.git "$PATH_INSTALL"
+echo "ok!"
 
 cd "$PATH_INSTALL"
 
+echo "install rust..."
 # Install Rust
 if ! command -v cargo >/dev/null; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
     source "$HOME/.cargo/env"
+    echo "installed!!"
 fi
 
+echo "install bun..."
 # Install Bun
 if ! command -v bun >/dev/null; then
     curl -fsSL https://bun.sh/install | bash
     export PATH="$HOME/.bun/bin:$PATH"
+    echo "installed!!"
 fi
 
+echo "install dependencies..."
 # Install dependencies
 bun install
 
+echo "running..."
 # Run
 bun tauri dev
